d8793422ede4921856104e550d7e4b4ba059a576
Alessandra Lamas Luisi <lecaluisi@hotmail.com>
2021-09-12 20:34:27 -0300

    Finalização

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-function App() {
+import React from "react";
+import Layout from "./components/layout";
+import NoSearch from "./components/no-search";
+import Profile from "./components/profile";
+import Repositories from "./components/repositories";
+import useGithub from "./hooks/github-hooks";
+
+const App = () => {
+  const { githubState } = useGithub();
   return (
-    <div className="App">
-      Início
-    </div>
+    <Layout>
+      {githubState.hasUser ? (
+        <>
+          {githubState.loading ? (
+            <p>Loading</p>
+          ) : (
+            <>
+              <Profile />
+              <Repositories />
+            </>
+          )}
+        </>
+      ) : (
+        <NoSearch />
+      )}
+    </Layout>
   );
-}
+};
 
 export default App;
